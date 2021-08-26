1ce5d0aedf08b8acccd8a749318b4bc8638f7197
SLL <qgh114@alumni.ku.dk>
2021-08-26 11:10:01 +0200

    ex.

--- 1.intro/Main.java
+++ 1.intro/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
     public static void main(String[] args){
+    Main main=new Main();
+    main.test("test");
 
+    }
+    public static void test(String hello){
+        System.out.println(hello);
     }
 
 }
