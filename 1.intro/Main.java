e56f6a52c94282ada46ccda661e5f05236dec5ac
SLL <qgh114@alumni.ku.dk>
2021-08-26 11:45:52 +0200

    exercise

--- 1.intro/Main.java
+++ 1.intro/Main.java
@@ -3,10 +3,21 @@ public class Main {
     public static void main(String[] args){
     Main main=new Main();
     main.test("test");
+    //Systems.test("Say hello");
+    //Systems.test2(5);
+    Systemas.out.println("Say hello");
+    Systemas.out.println(5);
+    Systemas.out.println(5.00);
+    Systemas.out.println(5.0);
+
+
+
 
     }
+
     public static void test(String hello){
         System.out.println(hello);
+
     }
 
 }
---
+++ 1.intro/Systemas.java
@@ -0,0 +1,21 @@
+public class Systemas {
+
+    public static class out {
+
+        public static void println(String hello) {
+            System.out.println(hello);
+        }
+
+        public static void println(int hello) {
+            System.out.println(hello);
+        }
+
+        public static void println(double hello) {
+            System.out.println(hello);
+        }
+
+        public static void println(float hello) {
+            System.out.println(hello);
+        }
+    }
+}
