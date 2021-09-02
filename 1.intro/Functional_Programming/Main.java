21dfeb0b9e27d83323db896121fa1957bee01a34
SLL <qgh114@alumni.ku.dk>
2021-09-02 13:45:03 +0200

    functional

--- 1.intro/Functional_Programming/Main.java
+++ 1.intro/Functional_Programming/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         AbstractClass abstractClassImplemented = new AbstractClass(){
-            @Override
+
             public void makeSurrealArt(){
                 System.out.println("Created by ");
 
             }
         };
+
 
     }
 }
