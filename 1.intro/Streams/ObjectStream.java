163d02f3e90fb629b857a170fcc441812d807148
SLL <qgh114@alumni.ku.dk>
2021-09-07 11:25:52 +0200

    string streams + number streams

--- 1.intro/Streams/ObjectStream.java
+++ 1.intro/Streams/ObjectStream.java
@@ -1,2 +1,13 @@
+import java.util.Date;
+import java.util.stream.Stream;
+
 public class ObjectStream {
+    public static void main(String[] args) {
+        Stream<Date> clock = Stream.generate(Date::new);
+        // clock.forEach(timestamp -> System.out.println(timestamp));
+
+        // Alternative syntax
+        Stream<Date> clock = Stream.generate(() -> new Date());
+        // clock.forEach(timestamp -> System.out.println(timestamp));
+    }
 }
