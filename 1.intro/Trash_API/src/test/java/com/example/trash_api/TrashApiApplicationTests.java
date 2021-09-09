8d466c872fb1d55255a34f4dc24716b23e698520
SLL <qgh114@alumni.ku.dk>
2021-09-09 11:47:52 +0200

    Spring

---
+++ 1.intro/Trash_API/src/test/java/com/example/trash_api/TrashApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trash_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
