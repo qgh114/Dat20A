107292a087fb36278d67348090bb87b4eeaf2d3e
SLL <qgh114@alumni.ku.dk>
2021-09-21 10:31:22 +0200

    HandMaven & Paintings(dinosaurs)

---
+++ 1.intro/TheHandMavensTale/src/main/java/HandMavens/controllers/HandMavenController.java
@@ -0,0 +1,14 @@
+package HandMavens.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HandMavenController {
+
+    @GetMapping("/about")
+    public String aboutProject(){
+        return "about";
+
+    };
+}
