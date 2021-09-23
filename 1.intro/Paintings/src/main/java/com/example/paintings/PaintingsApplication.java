764c936c3bfba2582e179b95c4240cc1d49ce947
SLL <qgh114@alumni.ku.dk>
2021-09-23 11:58:58 +0200

    HandMaven & Paintings(dinosaurs)

--- 1.intro/Paintings/src/main/java/com/example/paintings/PaintingsApplication.java
+++ 1.intro/Paintings/src/main/java/com/example/paintings/PaintingsApplication.java
@@ -8,7 +8,7 @@ public class PaintingsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PaintingsApplication.class, args);
-        System.out.println("hello");
+        System.out.println("hellog");
     }
 
 }
